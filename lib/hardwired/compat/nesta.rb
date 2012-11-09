3fd74dfdcd6211d6649463eb1796dfe2dc5c0825
Nathanael Jones <nathanael.jones@gmail.com>
2012-11-08 22:47:58 -0500

    Added nesta breadcrumb methods

--- lib/hardwired/compat/nesta.rb
+++ lib/hardwired/compat/nesta.rb
@@ -19,6 +19,37 @@ module Hardwired
     before '*' do
     	Nesta::Config = settings
     end
+
+
+    helpers do
+    	def breadcrumb_ancestors
+        ancestors = []
+        page = @page
+        while page
+          ancestors << page
+          page = page.parent
+        end
+        ancestors.reverse
+      end
+
+      def display_breadcrumbs(options = {})
+        haml_tag :ul, :class => options[:class] do
+          breadcrumb_ancestors[0...-1].each do |page|
+            haml_tag :li do
+              haml_tag :a, :<, :href => path_to(page.abspath), :itemprop => 'url' do
+                haml_tag :span, :<, :itemprop => 'title' do
+                  haml_concat link_text(page)
+                end
+              end
+            end
+          end
+          haml_tag(:li, :class => current_breadcrumb_class) do
+            haml_concat link_text(@page)
+          end
+        end
+      end
+     end
+
   end
 end
 
