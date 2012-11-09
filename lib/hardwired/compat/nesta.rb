8966dae44a44cb3b374a5c7690fe5ddba55b0aa4
Nathanael Jones <nathanael.jones@gmail.com>
2012-11-09 00:49:14 -0500

    Added more nesta compatibility tags

--- lib/hardwired/compat/nesta.rb
+++ lib/hardwired/compat/nesta.rb
@@ -17,11 +17,53 @@ module Hardwired
 
    	## Make Nesta::Config work
     before '*' do
-    	Nesta::Config = settings
+    	Nesta::Config = settings unless defined?(Nesta::Config)
     end
 
 
+
     helpers do
+
+    	def url_for(page)
+        File.join(base_url, page.path)
+      end
+  
+      def base_url
+        url = "http://#{request.host}"
+        request.port == 80 ? url : url + ":#{request.port}"
+      end
+  
+      def absolute_urls(text)
+        text.gsub!(/(<a href=['"])\//, '\1' + base_url + '/')
+        text
+      end
+  
+      def nesta_atom_id_for_page(page)
+        published = page.date.strftime('%Y-%m-%d')
+        "tag:#{request.host},#{published}:#{page.abspath}"
+      end
+  
+      def atom_id(page = nil)
+        if page
+          page.atom_id || nesta_atom_id_for_page(page)
+        else
+          "tag:#{request.host},2009:/"
+        end
+      end
+  
+      def format_date(date)
+        date.strftime("%d %B %Y")
+      end
+ 
+
+      def local_stylesheet_link_tag(name)
+        pattern = File.expand_path(Hardwired.Paths.content_path("/css/#{name}.s{a,c}?ss"))
+        if Dir.glob(pattern).size > 0
+          haml_tag :link, :href => "/css/#{name}.css", :rel => "stylesheet"
+        end
+      end
+
+
     	def breadcrumb_ancestors
         ancestors = []
         page = @page
