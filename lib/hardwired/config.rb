6cf05dd6671f8fd7e1cd2a491c134b7e77efde16
Nathanael Jones <nathanael.jones@gmail.com>
2013-01-30 15:06:30 +0100

    Added Hardwired::Config.default_config, and log message when not running in production.

--- lib/hardwired/config.rb
+++ lib/hardwired/config.rb
@@ -2,8 +2,25 @@ module Hardwired
   module Config
     class << self
 
+      attr_writer :default_config
+
+      def default_config
+        @defaut_config || {
+          haml:{format: :html5},
+          production: {
+            sass:{style: :compressed},
+            less:{compress: true},
+          },
+          development:{
+            sass:{style: :expanded},
+            less:{compress: false},
+
+          }
+        }
+      end
+
       def config
-        @@config
+        @config
       end 
 
       # Loads the configuration from the YAML files whose +paths+ are passed as
@@ -13,9 +30,10 @@ module Hardwired
         Dir.chdir(Hardwired::Paths.root || '.') do
           Dir.glob(path) do |file|
             $stderr.puts "loading config file '#{file}'" if app.logging?
+            $stderr.puts "Running in environment mode #{app.environment}" unless app.environment == :production
             document = IO.read(file)
-            @@config = RecursiveOpenStruct.new(config_for_env(YAML.load(document),app.environment) || {})
-            return @@config 
+            @config = RecursiveOpenStruct.new(config_for_env(default_config.merge(YAML.load(document)),app.environment) || {})
+            return @config 
           end
         
         end
